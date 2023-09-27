d47ab3a0fd75b2a07fce0f5a07ccccdfadfa88dc
Italis Odilson Woodly <italisodilsonwoodly@hotmail.com>
2023-09-27 12:18:30 -0400

    Iteration4: form_letter

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,27 +1,34 @@
 require 'csv'
 require 'google/apis/civicinfo_v2'
-
+require 'erb'
 
 def clean_zipcode(zipcode)
-  zipcode.to_s.rjust(5, '0')[0..4]
+  zipcode.to_s.rjust(5,"0")[0..4]
 end
 
 def legislators_by_zipcode(zip)
-  civic_info = Google::Apis::CivicinfoV2::CivicInfoService.new
-  civic_info.key = 'AIzaSyClRzDqDh5MsXwnCWi0kOiiBivP6JsSyBw'
+    civic_info = Google::Apis::CivicinfoV2::CivicInfoService.new
+    civic_info.key = 'AIzaSyClRzDqDh5MsXwnCWi0kOiiBivP6JsSyBw'
+  
+    begin
+      civic_info.representative_info_by_address(
+        address: zip,
+        levels: 'country',
+        roles: ['legislatorUpperBody', 'legislatorLowerBody']
+      ).officials
+    rescue
+      'You can find your representatives by visiting www.commoncause.org/take-action/find-elected-officials'
+    end
+end
 
-  begin
-    legislators = civic_info.representative_info_by_address(
-      address: zip,
-      levels: 'country',
-      roles: ['legislatorUpperBody', 'legislatorLowerBody']
-    )
-    legislators = legislators.officials
-    legislator_names = legislators.map(&:name)
-    legislator_names.join(", ")
-  rescue
-    'You can find your representatives by visiting www.commoncause.org/take-action/find-elected-officials'
-  end
+def save_thank_you_letter(id,form_letter)
+    Dir.mkdir('output') unless Dir.exist?('output')
+
+    filename = "output/thanks_#{id}.html"
+  
+    File.open(filename, 'w') do |file|
+      file.puts form_letter
+    end
 end
 
 puts 'EventManager initialized.'
@@ -32,6 +39,9 @@ contents = CSV.open(
   header_converters: :symbol
 )
 
+template_letter = File.read('form_letter.erb')
+erb_template = ERB.new template_letter
+
 contents.each do |row|
   name = row[:first_name]
 
@@ -39,6 +49,8 @@ contents.each do |row|
 
   legislators = legislators_by_zipcode(zipcode)
 
-  puts "#{name} #{zipcode} #{legislators}"
-end
+  form_letter = erb_template.result(binding)
 
+  save_thank_you_letter(row[:id], form_letter)
+
+end
