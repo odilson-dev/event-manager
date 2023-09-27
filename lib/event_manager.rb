36e8b372a21689894aaf4328fb00bc9173cf7cbe
Italis Odilson Woodly <italisodilsonwoodly@hotmail.com>
2023-09-27 14:43:56 -0400

    Implement clean_phone_number method

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -31,6 +31,22 @@ def save_thank_you_letter(id,form_letter)
     end
 end
 
+def clean_phone_number(phone_number)
+    if phone_number.count("0123456789") < 10
+        ""
+    elsif phone_number.count("0123456789") == 10
+        phone_number
+    elsif phone_number.count("0123456789") == 11
+        if phone_number[0] == "1"
+            phone_number[1..10]
+        else
+            ""
+        end
+    elsif phone_number.count("0123456789") > 11
+        ""
+    end
+end
+
 puts 'EventManager initialized.'
 
 contents = CSV.open(
@@ -42,15 +58,23 @@ contents = CSV.open(
 template_letter = File.read('form_letter.erb')
 erb_template = ERB.new template_letter
 
+# contents.each do |row|
+#   name = row[:first_name]
+
+#   zipcode = clean_zipcode(row[:zipcode])
+
+#   legislators = legislators_by_zipcode(zipcode)
+
+#   form_letter = erb_template.result(binding)
+
+#   save_thank_you_letter(row[:id], form_letter)
+
+# end
+numbers = ("0".."9").to_a
 contents.each do |row|
-  name = row[:first_name]
-
-  zipcode = clean_zipcode(row[:zipcode])
-
-  legislators = legislators_by_zipcode(zipcode)
-
-  form_letter = erb_template.result(binding)
-
-  save_thank_you_letter(row[:id], form_letter)
-
-end
+    phone_number = clean_phone_number(row[:homephone])
+    
+ 
+    puts "#{phone_number} length => #{phone_number.count('0123456789')}"
+  
+  end
